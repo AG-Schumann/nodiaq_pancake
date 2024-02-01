a760029f8d564e2e5f38cda06066b59ff7c5e5e1
jarongrigat <jaron.grigat@googlemail.com>
2024-02-01 16:03:59 +0100

    login router

---
+++ routes/login.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var url = require("url");
+var router = express.Router();
+var common = require('./common');
+
+
+router.get('/', function(req, res) {
+    let config = common.GetRenderConfig(req);
+    res.render('options', config);
+});
